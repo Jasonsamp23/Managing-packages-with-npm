29bc53e41770b1140298573956e329074f3cb2ff
Jason Sampson <Jsampsonn19@gmail.com>
2025-04-09 13:35:44 -0400

    sending html file

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ if (!process.env.DISABLE_XORIGIN) {
 
 //Listen on port set in environment variable or default to 3000
 app.get("/", function(req,res){
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 const port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port,function () {
