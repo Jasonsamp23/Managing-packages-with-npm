e319ef14474847b9e112eca2feaf20e5b119c022
Jason Sampson <Jsampsonn19@gmail.com>
2025-04-09 14:14:34 -0400

    serving json file to server

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.use('/public', express.static(__dirname + "/public"));
 //Serving JSON on a specific route 
 
 app.get("/json", function(req,res){
-  res.json({"message": "Hello Json"})
+  res.json(
+    {"message": "Hello Json"}
+  );
 });
 
 // app.use('/public', express.static(process.cwd() + '/public'));
